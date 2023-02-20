211a7c78f22e1712eaaf1229c20546d0bd851133
Boris <adboris@yandex.ru>
2023-02-20 09:52:48 +0300

    cors

--- app.ts
+++ app.ts
@@ -1,9 +1,12 @@
 import express from 'express'
 import { router } from './routes/todo.route'
+import cors from 'cors'
 
 const app = express()
 
+
 app.use(express.json())
+app.use(cors())
 app.use('/api',router)
 
 app.listen(3004,() => console.log('Server started, port: 3004'))
